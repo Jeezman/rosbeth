546bdbbcd49997653e627bc9573c3a235c0b044f
sashlee <sarahrichie08@gmail.com>
2022-06-29 16:29:07 +0100

    implemented both hero-section and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script>
 import { RouterLink, RouterView } from "vue-router";
 import Navigation from "./components/Navigation.vue";
+import Home from "./pages/Home.vue";
 // import HelloWorld from "@/components/HelloWorld.vue";
 export default {
   name: "App",
   components: {
     Navigation,
+    Home,
   },
 };
 </script>
 
 <template>
   <Navigation />
+  <Home />
   <div class="container">
     <div class="row">
       <div class="col-md-12 mt-8">
---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<script>
+import Carousel from "../components/Carousel.vue";
+export default {
+  name: "Home",
+  components: {
+    Carousel,
+  },
+};
+</script>
+
+<template>
+  <Carousel />
+</template>
+
+<style></style>
